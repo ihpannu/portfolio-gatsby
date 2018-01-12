00222be68642f2c0f8d6921f6de11151c99c1367
iharmanpannu <iharmanpannu@gmail.com>
2018-01-12 15:20:48 -0800

    changes to index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import {SiteHeader} from '../components/Header/Header'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/about/">Go to page 2</Link>
-  </div>
-)
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/about/">Go to page 2</Link>
+    </div>
+);
 
 export default IndexPage
