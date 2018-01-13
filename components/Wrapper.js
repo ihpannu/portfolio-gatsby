49346ae038537c56a2463acfb7fb39a3a0a874db
iharmanpannu <iharmanpannu@gmail.com>
2018-01-13 11:28:28 -0800

    added styles

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
   max-width: 1024px;
   margin: auto;
   padding: 0 1em;
+  color: #333233;
+  background: #FFFEFF;
   display: flex;
   flex-direction: column;
   justify-content: center;
