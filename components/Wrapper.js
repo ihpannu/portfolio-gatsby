87e899debbd627a5d8860f7b20a2cb6ae212da3b
iharmanpannu <iharmanpannu@gmail.com>
2018-01-13 11:53:30 -0800

    added component

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components'
 
+const sizes = {
+  fullpage: 1170,
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+
+  return acc
+}, {})
+
+
+
+
 const Wrapper = styled.section`
   max-width: 1024px;
   margin: auto;
@@ -11,5 +32,7 @@ const Wrapper = styled.section`
   justify-content: center;
   align-content: center;
 
-  h1
+  ${media.tablet`background: green`}
 `;
+
+export {media, Wrapper}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Link from 'gatsby-link'
 
 import {SiteHeader} from '../../components/Header'
+import {Wrapper} from '../../components/Wrapper'
+
+
+export default class IndexPage extends Component {
+  render() {
+    return
+      <Wrapper>
+        <SiteHeader>Hello</SiteHeader>
+        <h1>I am Harman Pannu</h1>
+      </Wrapper>
+  }
+}
+
+// const IndexPage = () => (
+//   <Wrapper>
 
 
 
-const IndexPage = () => (
-  <div>
-    <SiteHeader>Hello</SiteHeader>
-      <h1>I am Harman Pannu</h1>
+//   </Wrapper>
+// );
 
-  </div>
-);
-
-export default IndexPage
+// export default IndexPage
